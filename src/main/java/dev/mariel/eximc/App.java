b8de6982b850ff07529e62a8ae0df10738ccab1d
Codebreakerblack <blancohmariel@gmail.com>
2024-11-20 00:44:22 +0100

    add logica to Class App

--- src/main/java/dev/mariel/eximc/App.java
+++ src/main/java/dev/mariel/eximc/App.java
@@ -1,17 +1,37 @@
 package dev.mariel.eximc;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
-    }
+import java.util.Scanner;
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+import dev.mariel.eximc.services.ImcCalculator;
+import dev.mariel.eximc.services.ImcEvaluator;
+
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ImcCalculator imcCalculator = new ImcCalculator();
+        ImcEvaluator imcEvaluator = new ImcEvaluator();
+        Scanner scanner = new Scanner(System.in);
+
+        try{
+            System.out.println("Welcome to the IMC calculation program");
+            System.out.println("\nPlease, enter your weight in kilograms: ");
+            double weight = scanner.nextDouble();
+
+            System.out.println("\nPlease, enter your height in meters: ");
+            double height = scanner.nextDouble();
+
+            double imc = imcCalculator.calculateIMC(weight, height);
+
+            String result = imcEvaluator.classifyImc(imc);
+
+            System.out.println("\nYour IMC is: " + String.format("%.2f", imc));
+            System.out.println("\nClassification: " + result);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("\nError: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("\nAn unexpected error occurred. Please try again");
+        } finally {
+            scanner.close();
+        }
     }
 }
