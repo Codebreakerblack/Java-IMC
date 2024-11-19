e10030fd45d638ca1adf43d1f2d35a39287c37bd
Codebreakerblack <blancohmariel@gmail.com>
2024-11-19 23:08:13 +0100

    add class ImcCalculator

---
+++ src/main/java/dev/mariel/eximc/App.java
@@ -0,0 +1,17 @@
+package dev.mariel.eximc;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/dev/mariel/eximc/services/ImcCalculator.java
@@ -0,0 +1,11 @@
+package dev.mariel.eximc.services;
+
+public class ImcCalculator {
+    public double calculateIMC(double weight, double height) {
+        if (height <= 0) {
+            throw new IllegalArgumentException("Height must be greater than 0");
+        }
+        return weight / Math.pow(height, 2);
+    }
+
+}
